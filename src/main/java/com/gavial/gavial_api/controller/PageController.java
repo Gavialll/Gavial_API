6546e40ac6f9dd9bbcb19a8920101ee4782ba45b
andriudutko@gmail.com <andriudytko@gmail.com>
2022-02-21 01:34:48 +0200

    add Procfile

--- src/main/java/com/gavial/gavial_api/controller/PageController.java
+++ src/main/java/com/gavial/gavial_api/controller/PageController.java
@@ -2,29 +2,22 @@ package com.gavial.gavial_api.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class PageController {
 
     @GetMapping("/exchange/getExchange")
     public String index(){
-        return "PWA/exchange/exchange";
+        return "exchange";
     }
 
     @GetMapping("/")
     public String indexs(){
-        return "PWA/exchange/exchange";
+        return "index";
     }
 
     @GetMapping("/exchange/getHistory")
     public String indexe(){
-        return "PWA/exchange/history";
-    }
-
-
-    @GetMapping("/eee")
-    public String indexess(){
-        return "dc";
+        return "history";
     }
 }
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,20 +1,86 @@
+function GET(url, headers = {
+                        'Content-Type': 'application/json'
+                    }) {
+    return fetch(url, {
+        method: "GET",
+        headers: headers
+    }).then(response => {
+        if (response.ok) {
+            return response.json()
+        }
+        else {
+            console.log('false')
+        }
+    })
+}
 
-let canvas = document.getElementById('canvas');
-let element = document.createElement('div');
-    element.classList.add('element');
-// let left = document.createElement('div');
-//     left.classList.add('side');
-//     left.innerText = '20.22';
-// let center = document.createElement('div');
-//     center.classList.add('center');
-//     center.innerText = 'USD';
-// let right = document.createElement('div');
-//     right.classList.add('side');
-//     right.innerText = '20.22';
-// canvas.append(element);
-// element.append(left, center, right);
-//
-// let date = new Date().toLocaleDateString();
+let url = '/api/exchange';
+
+let first = [];
+let second = [];
+GET(url).then(res => {
+    let canvas = document.getElementById('canvas');
+    let button = document.getElementById('more')
+
+    let arr = res.exchangeRate;
+    console.log(arr)
+    let currency = ['USD', 'EUR', 'PLN', 'CZK'];
+
+
+    for (let j = 0; j < currency.length; j++) {
+        for (let i = 0; i < arr.length; i++) {
+                if (arr[i].currency === currency[j]) {
+                    first.push(arr[i]);
+                    arr.slice(i,i)
+                }
+        }
+    }
+    second = arr;
+    for (let i = 0; i < first.length; i++) {
+            let element = document.createElement('div');
+            element.classList.add('element');
+            let left = document.createElement('div');
+            left.classList.add('side');
+            left.innerText = first[i].purchaseRate.toString().substr(0, 5);
+            let center = document.createElement('div');
+            center.classList.add('center');
+            center.innerText = first[i].currency;
+            let right = document.createElement('div');
+            right.classList.add('side');
+            right.innerText = first[i].saleRate.toString().substr(0, 5);
+
+            canvas.insertBefore(element, button);
+            element.append(left, center, right);
+        }
+})
+
+let r = 0;
+let more = document.getElementById("more");
+more.addEventListener('click', () => {
+    let canvas = document.getElementById('canvas');
+    console.log(second);
+    for (let i = 1; i < second.length; i++) {
+        let element = document.createElement('div');
+        element.classList.add('element');
+        let left = document.createElement('div');
+        left.classList.add('side');
+        left.innerText = second[i].purchaseRateNB.toString().substr(0, 5);
+        let center = document.createElement('div');
+        center.classList.add('center');
+        center.innerText = second[i].currency;
+        let right = document.createElement('div');
+        right.classList.add('side');
+        right.innerText = second[i].saleRateNB.toString().substr(0, 5);
+
+        canvas.insertBefore(element, more);
+        element.append(left, center, right);
+        r++;
+        console.log(r)
+    }
+})
+
+
+
 
 let right = document.getElementById("right");
 let left = document.getElementById("left");
