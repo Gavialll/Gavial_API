3e25d331d353560753463c83be4111d05068a9f2
andriudutko@gmail.com <andriudytko@gmail.com>
2022-03-19 16:57:54 +0200

    fixing modal windows scroll

--- src/main/resources/static/js/module.js
+++ src/main/resources/static/js/module.js
@@ -1,5 +1,5 @@
 
 export function address(namePage){
-    return `http://localhost:63342/Gavial_API/templates${namePage}.html?_ijt=m41rs8hj7qt1n2lquoeg6l4pie&_ij_reload`;
-    // return `https://gavialapi.herokuapp.com${namePage}`;
+    // return `http://localhost:63342/Gavial_API/templates${namePage}.html?_ijt=m41rs8hj7qt1n2lquoeg6l4pie&_ij_reload`;
+    return `https://gavialapi.herokuapp.com${namePage}`;
 }
