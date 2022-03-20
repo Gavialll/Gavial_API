d15e70800a60c4373024d906bf3ccbdf3704149e
andriudutko@gmail.com <andriudytko@gmail.com>
2022-03-20 13:45:16 +0200

    admin sentence complete

--- src/main/java/com/gavial/gavial_api/model/Sentence.java
+++ src/main/java/com/gavial/gavial_api/model/Sentence.java
@@ -16,7 +16,7 @@ import javax.persistence.Id;
 public class Sentence {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String english;
     private String ukraine;
